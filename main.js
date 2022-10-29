670bd2017d94b38a90ed559a530725bdd784edd8
ManeeshaMaduranga <MadurangaBandara1006@gmail.com>
2022-10-29 20:46:44 +0530

    Add to list students

---
+++ main.js
@@ -0,0 +1,81 @@
+//Friend Class
+
+class Friend{
+   
+   constructor(name,age,email){
+      this.name = name;
+      this.age = age;
+      this.email = email;
+   }
+
+}
+
+
+
+//UI Class
+
+class UI{
+
+   static displayFreinds(){
+      
+      const friends = [
+         {
+            name : "Maneesha Maduranga",
+            age : 23,
+            email : 'Maneeshamaduranga@gmail.com'
+         },
+         {
+            name : "Chamod Danajaya",
+            age : 23,
+            email : 'Chamod@gmail.com'
+         },
+         {
+            name : "Vijth Dinesh",
+            age : 23,
+            email : 'Vijith@gmail.com'
+         }
+      ]
+
+      const freind = friends;
+
+      freind.forEach(element => {
+         UI.addFriends(element)
+      });
+
+      
+
+   }
+
+   static addFriends(fre){
+      const list = document.getElementById("friend-list");
+      const row = document.createElement("tr");
+
+      row.innerHTML = `
+         <td>${fre.name}</td>
+         <td>${fre.age}</td>
+         <td>${fre.email}</td>
+         <td><button class="btn btn-success" type="button">Delete</button></td>
+
+      `;
+
+      list.appendChild(row);
+
+   }
+
+}
+
+
+
+
+
+
+
+//Event Display Friends
+
+document.addEventListener("DOMContentLoaded",UI.displayFreinds);
+
+
+//Event add friends
+
+
+//Event remove Friends
